603dbe4759008fee8f837a59cec5af2f3962b4f6
Elias <30868938+Makoto7@users.noreply.github.com>
2019-02-19 12:26:55 +0100

    Update my_put_neg.c

--- my_put_neg.c
+++ my_put_neg.c
@@ -1,13 +1,3 @@
-/*
-** my_put_neg.c for neg in /home/abouss_e/rendu/PSU_2015_my_printf
-**
-** Made by elias aboussair
-** Login   <abouss_e@epitech.net>
-**
-** Started on  Tue Apr 12 22:48:06 2016 elias aboussair
-** Last update Sat Apr 16 13:13:48 2016 elias aboussair
-*/
-
 #include "my.h"
 
 int	my_put_neg(int nb)
