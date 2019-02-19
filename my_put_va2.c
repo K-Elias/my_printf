30cb00975e19025349592a94cecbac2b3f4492ac
Elias <30868938+Makoto7@users.noreply.github.com>
2019-02-19 12:26:30 +0100

    Update my_put_va2.c

--- my_put_va2.c
+++ my_put_va2.c
@@ -1,13 +1,3 @@
-/*
-** my_put_va2.c for azerty in /home/abouss_e/rendu/PSU_2015_my_printf
-**
-** Made by elias aboussair
-** Login   <abouss_e@epitech.net>
-**
-** Started on  Fri Apr 15 19:29:07 2016 elias aboussair
-** Last update Sat Apr 16 17:37:02 2016 elias aboussair
-*/
-
 #include "my.h"
 
 int	my_put_hex_nbr_va(va_list va)
