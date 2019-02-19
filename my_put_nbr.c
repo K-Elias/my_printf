237d86f366b9048a2026fc79dfcbd4482cdfff1a
Elias <30868938+Makoto7@users.noreply.github.com>
2019-02-19 12:25:53 +0100

    Update my_put_nbr.c

--- my_put_nbr.c
+++ my_put_nbr.c
@@ -1,13 +1,3 @@
-/*
-** my_put_nbr.c for put_nbr in /home/abouss_e/rendu/PSU_2015_my_printf
-**
-** Made by elias aboussair
-** Login   <abouss_e@epitech.net>
-**
-** Started on  Mon Apr 11 18:21:54 2016 elias aboussair
-** Last update Sun Apr 17 18:06:32 2016 elias aboussair
-*/
-
 #include "my.h"
 
 int		my_put_nbr(int nb)
