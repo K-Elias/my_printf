86b015afe877ea1a006d6f1cfa96440baccd555a
Elias <30868938+Makoto7@users.noreply.github.com>
2019-02-19 12:26:21 +0100

    Update my_put_va.c

--- my_put_va.c
+++ my_put_va.c
@@ -1,13 +1,3 @@
-/*
-** my_put_list.c for viosdfovihdf in /home/abouss_e/rendu/PSU_2015_my_printf
-**
-** Made by elias aboussair
-** Login   <abouss_e@epitech.net>
-**
-** Started on  Fri Apr 15 18:50:41 2016 elias aboussair
-** Last update Sat Apr 16 16:27:07 2016 elias aboussair
-*/
-
 #include "my.h"
 
 int	my_putchar_va(va_list va)
