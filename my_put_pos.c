a573cb9ba1c00e95d0989c8341c6450d946faabe
Elias <30868938+Makoto7@users.noreply.github.com>
2019-02-19 12:29:59 +0100

    Update my_put_pos.c

--- my_put_pos.c
+++ my_put_pos.c
@@ -1,13 +1,3 @@
-/*
-** my_put_pos.c for pos in /home/abouss_e/rendu/PSU_2015_my_printf
-**
-** Made by elias aboussair
-** Login   <abouss_e@epitech.net>
-**
-** Started on  Sat Apr 16 21:42:24 2016 elias aboussair
-** Last update Sat Apr 16 22:08:35 2016 elias aboussair
-*/
-
 #include "my.h"
 
 unsigned int	my_put_pos(unsigned int nb)
