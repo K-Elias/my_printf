a5e03dc8a1453cb5e8251968cf0e00426b3cccda
Elias <30868938+Makoto7@users.noreply.github.com>
2019-02-19 12:28:11 +0100

    Update my_put_pos_va.c

--- my_put_pos_va.c
+++ my_put_pos_va.c
@@ -1,13 +1,3 @@
-/*
-** my_put_pos_va.c for vapos in /home/abouss_e/rendu/PSU_2015_my_printf
-**
-** Made by elias aboussair
-** Login   <abouss_e@epitech.net>
-**
-** Started on  Sat Apr 16 22:02:16 2016 elias aboussair
-** Last update Sat Apr 16 22:15:48 2016 elias aboussair
-*/
-
 #include "my.h"
 
 int	my_put_pos_va(va_list va)
