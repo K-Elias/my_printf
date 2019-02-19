24e097c5a0335a3507e34caeb39c09441fb9b41c
Elias <30868938+Makoto7@users.noreply.github.com>
2019-02-19 12:23:40 +0100

    Update my_put_ptr.c

--- my_put_ptr.c
+++ my_put_ptr.c
@@ -1,13 +1,3 @@
-/*
-** my_put_ptr.c for putadr in /home/abouss_e/rendu/PSU_2015_my_printf
-**
-** Made by elias aboussair
-** Login   <abouss_e@epitech.net>
-**
-** Started on  Sat Apr 16 22:41:06 2016 elias aboussair
-** Last update Sun Apr 17 22:22:26 2016 elias aboussair
-*/
-
 #include "my.h"
 
 int	my_put_ptr(unsigned long nb)
