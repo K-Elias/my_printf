034418ea65a66633b7bc9e079e608231655e4e67
Elias <30868938+Makoto7@users.noreply.github.com>
2019-02-19 12:27:43 +0100

    Update my_put_ptr_va.c

--- my_put_ptr_va.c
+++ my_put_ptr_va.c
@@ -1,13 +1,3 @@
-/*
-** my_put_ptr_va.c for azerty in /home/abouss_e/rendu/PSU_2015_my_printf
-**
-** Made by elias aboussair
-** Login   <abouss_e@epitech.net>
-**
-** Started on  Sun Apr 17 15:48:22 2016 elias aboussair
-** Last update Sun Apr 17 23:20:53 2016 elias aboussair
-*/
-
 #include "my.h"
 
 int	my_put_ptr_va(va_list va)
