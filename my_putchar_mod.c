c25e362a10ee5c613511ac927f27a11bcfb7036a
Elias <30868938+Makoto7@users.noreply.github.com>
2019-02-19 12:27:58 +0100

    Update my_putchar_mod.c

--- my_putchar_mod.c
+++ my_putchar_mod.c
@@ -1,13 +1,3 @@
-/*
-** my_putchar_mod.c for poi in /home/abouss_e/rendu/PSU_2015_my_printf
-**
-** Made by elias aboussair
-** Login   <abouss_e@epitech.net>
-**
-** Started on  Sun Apr 17 18:43:46 2016 elias aboussair
-** Last update Sun Apr 17 18:44:42 2016 elias aboussair
-*/
-
 #include "my.h"
 
 void	my_putchar_mod(char c)
