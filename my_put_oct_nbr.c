23f2367c3a740461ec3ac144487af62c6a09a21d
Elias <30868938+Makoto7@users.noreply.github.com>
2019-02-19 12:24:39 +0100

    Update my_put_oct_nbr.c

--- my_put_oct_nbr.c
+++ my_put_oct_nbr.c
@@ -1,13 +1,3 @@
-/*
-** my_put_hex_nbr.c for hexnbr in /home/abouss_e/rendu/PSU_2015_my_printf
-**
-** Made by elias aboussair
-** Login   <abouss_e@epitech.net>
-**
-** Started on  Tue Apr 12 18:03:02 2016 elias aboussair
-** Last update Sun Apr 17 20:08:28 2016 elias aboussair
-*/
-
 #include "my.h"
 
 int		my_put_oct_nbr(unsigned int nb)
