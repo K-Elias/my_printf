c1e34645fac4f4f81e0da560f84dca3c9f916f8a
Elias <30868938+Makoto7@users.noreply.github.com>
2019-02-19 12:25:39 +0100

    Update epur_sp.c

--- epur_sp.c
+++ epur_sp.c
@@ -1,13 +1,3 @@
-/*
-** epur_str.c for ' ' in /home/abouss_e/rendu/PSU_2015_my_printf
-**
-** Made by elias aboussair
-** Login   <abouss_e@epitech.net>
-**
-** Started on  Sun Apr 17 16:44:49 2016 elias aboussair
-** Last update Sun Apr 17 18:29:44 2016 elias aboussair
-*/
-
 #include "my.h"
 
 int	epur_sp(const char *str, int i)
